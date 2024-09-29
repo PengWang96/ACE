dfedfbe2ffce724b3b63beabc2af320ee046d695
PengWang96 <pengwang1996@hotmail.com>
2024-09-29 09:45:36 +0800

    Update

--- R/ACE.R
+++ R/ACE.R
@@ -67,6 +67,7 @@ ACE <- function(Z, X, H0_indicator, gama = 0.05, h_max = 20, h_fix = NULL, reg =
       Y <- Z
       Z <- X
       X <- Y
+      n1 <- ncol(Z); n2 <- ncol(X); n <- n1
     }
 
     for (jy in 1:n1){
